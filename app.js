ad5d9590bec545ccbd416a80d2df52b042ad8e9e
Rafael Trevisan <rafaelst2000@gmail.com>
2020-09-01 18:31:09 -0300

    carrinho de compras

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    carrinho: [],
+    mensagemAlerta: 'Item adicionado!',
+    alertaAtivo: false,
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco
+        })
+      }
+      return total
+    }
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho)
+    }
   },
   filters: {
     numberToPrice(value) {
@@ -19,9 +38,39 @@ const vm = new Vue({
       fetch(`./api/produtos/${id}/dados.json`).then(res => res.json()).then(res => {
         this.produto = res
       })
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false
+    },
+    abrirModal(id) {
+      this.fetchProduto(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    adicionarItem() {
+      this.produto.estoque--
+      const { id, nome, preco } = this.produto
+      this.carrinho.push({ id, nome, preco })
+      this.alerta(`${nome} foi adicionado ao carrinho!`)
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1)
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) this.carrinho = JSON.parse(window.localStorage.carrinho)
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem
+      this.alertaAtivo = true
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500)
     }
   },
   created() {
     this.fetchProdutos()
+    this.checarLocalStorage()
   },
 })
